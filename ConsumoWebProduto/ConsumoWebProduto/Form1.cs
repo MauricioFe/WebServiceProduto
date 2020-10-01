cd95c605f178ea5de48aba74783e07505a4d5f5d
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-01 11:54:21 -0300

    Refatoração de código

--- ConsumoWebProduto/ConsumoWebProduto/Form1.cs
+++ ConsumoWebProduto/ConsumoWebProduto/Form1.cs
@@ -1,13 +1,5 @@
 ﻿using ConsumoWebProduto.ServiceProduto;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ConsumoWebProduto
